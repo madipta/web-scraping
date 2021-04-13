42b7a418401646295e3725b61854b6f876b705f8
madipta <madipta@gmail.com>
2021-04-13 08:54:04 +0700

    refactor

--- libs/data-access/src/lib/data-access.module.ts
+++ libs/data-access/src/lib/data-access.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
 import { DomainService } from "./domain/domain.service";
 import { LinkService } from "./link/link.service";
-import { ContentService } from './content/content.service';
+import { ContentService } from "./content/content.service";
 
 @Module({
   providers: [PrismaService, DomainService, LinkService, ContentService],
