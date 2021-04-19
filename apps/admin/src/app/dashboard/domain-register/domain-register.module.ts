2dc150ffefd9bff6605a7dc1d905353390c7ef7f
madipta <madipta@gmail.com>
2021-04-19 11:38:56 +0700

    create domain register form

--- apps/admin/src/app/dashboard/domain-register/domain-register.module.ts
+++ apps/admin/src/app/dashboard/domain-register/domain-register.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { Routes, RouterModule } from "@angular/router";
+import { NzPageHeaderModule } from "ng-zorro-antd/page-header";
+import { NzFormModule } from "ng-zorro-antd/form";
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from "ng-zorro-antd/input";
+import { NzSelectModule } from "ng-zorro-antd/select";
 import { DomainRegisterComponent } from "./domain-register.component";
 
 const routes: Routes = [
@@ -12,6 +18,16 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [DomainRegisterComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    NzPageHeaderModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
+    NzSelectModule,
+  ],
 })
 export class DomainRegisterModule {}
