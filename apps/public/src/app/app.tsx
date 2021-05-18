580b7ecaa4887f3ce01f5ce3f1c4cb5d6e244c8c
madipta <madipta@gmail.com>
2021-05-18 21:24:55 +0700

    layouting

--- apps/public/src/app/app.tsx
+++ apps/public/src/app/app.tsx
@@ -38,79 +38,79 @@ export function App() {
             </li>
           </ul>
         </nav>
-        <div className="flex gap-6 mb-16">
-          <div className="relative flex flex-1 overflow-hidden">
+        <div className="flex flex-col md:flex-row mb-16">
+          <div className="relative flex flex-1 overflow-hidden mx-auto mb-12 md:mb-0">
             <img src="assets/main.jpeg" alt=""></img>
-            <div className="absolute left-0 bottom-0 bg-gray-900 text-white text-base font-medium py-2 px-6 mb-4 mx-16 opacity-80">
+            <div className="absolute left-0 bottom-0 bg-gray-900 text-white text-sm md:text-base font-medium py-2 px-4 w-4/5 mb-4 opacity-80">
               test dululah yang agak panjang dikit biar kelihatan ukurannya pas
               atau nggak
             </div>
           </div>
-          <ul className="flex flex-col w-80">
-            <li className="flex gap-4 mb-4">
-              <img src="assets/hl1.jpeg" alt="" className="w-28"></img>
+          <ul className="flex flex-col w-80 md:ml-6">
+            <li className="flex mb-4">
+              <img src="assets/hl1.jpeg" alt="" className="w-28 mr-4"></img>
               <span className="flex flex-col flex-1">
                 VIDEO: Kemenangan Penuh Drama Jack Miller di MotoGP Prancis
               </span>
             </li>
-            <li className="flex gap-4 mb-4">
-              <img src="assets/hl2.jpeg" alt="" className="w-28"></img>
+            <li className="flex mb-4">
+              <img src="assets/hl2.jpeg" alt="" className="w-28 mr-4"></img>
               <span className="flex flex-col flex-1">
                 Mulai Besok Masuk DKI Pakai Surat Bebas Covid, Bukan SIKM
               </span>
             </li>
-            <li className="flex gap-4 mb-4">
-              <img src="assets/hl3.jpg" alt="" className="w-28"></img>
+            <li className="flex mb-4">
+              <img src="assets/hl3.jpg" alt="" className="w-28 mr-4"></img>
               <span className="flex flex-col flex-1">
                 Buruh Ancam Boikot Produk Indomaret Akibat Kasus THR 2020
               </span>
             </li>
-            <li className="flex gap-4 mb-4">
-              <img src="assets/hl4.jpeg" alt="" className="w-28"></img>
+            <li className="flex mb-4">
+              <img src="assets/hl4.jpeg" alt="" className="w-28 mr-4"></img>
               <span className="flex flex-col flex-1">
                 Antisipasi Lonjakan Covid, Pangdam-Kapolda Pantau Wisma Atlet
               </span>
             </li>
-            <li className="flex gap-4 mb-4">
-              <img src="assets/hl5.jpeg" alt="" className="w-28"></img>
+            <li className="flex mb-4">
+              <img src="assets/hl5.jpeg" alt="" className="w-28 mr-4"></img>
               <span className="flex flex-col flex-1">
                 Studi Ungkap Terapi Plasma Darah Pasien Corona Tidak Efektif
               </span>
             </li>
           </ul>
         </div>
-        <ul className="flex w-full items-stretch gap-4 pb-4 mb-16 overflow-x-hidden">
-          <li className="flex flex-none flex-col w-56 bg-gray-100 overflow-hidden">
+        <ul className="flex w-full items-stretch pb-4 mb-16 overflow-x-hidden">
+          <li className="flex flex-none flex-col bg-gray-100 w-60 mr-4 overflow-hidden">
             <img className="" src="assets/sub1.jpeg" alt=""></img>
             <div className="pt-2 px-3 pb-3">
               WhatsApp Akan Hadirkan Fitur Canggih, Disappearing Mode
             </div>
           </li>
-          <li className="flex flex-none flex-col w-56 bg-gray-100 overflow-hidden">
+          <li className="flex flex-none flex-col bg-gray-100 w-60 mr-4 overflow-hidden">
             <img className="" src="assets/sub2.jpeg" alt=""></img>
             <div className="pt-2 px-3 pb-3">
               Penduduk Gaza Tewas Akibat Serangan Israel Capai 197 Orang
             </div>
           </li>
-          <li className="flex flex-none flex-col w-56 bg-gray-100 overflow-hidden">
+          <li className="flex flex-none flex-col bg-gray-100 w-60 mr-4 overflow-hidden">
             <img className="" src="assets/sub3.jpeg" alt=""></img>
             <div className="pt-2 px-3 pb-3">
               13.675 Dites Acak saat Arus Balik, 72 Orang Reaktif Corona
             </div>
           </li>
-          <li className="flex flex-none flex-col w-56 bg-gray-100 overflow-hidden">
+          <li className="flex flex-none flex-col bg-gray-100 w-60 mr-4 overflow-hidden">
             <img className="" src="assets/sub4.jpeg" alt=""></img>
             <div className="pt-2 px-3 pb-3">
               Pegawai KPK Tak Lulus TWK Bakal Gugat SK Penonaktifan
             </div>
           </li>
-          <li className="flex flex-none flex-col w-56 bg-gray-100 overflow-hidden">
+          <li className="flex flex-none flex-col bg-gray-100 w-60 mr-4 overflow-hidden">
             <img className="" src="assets/sub5.jpeg" alt=""></img>
             <div className="pt-2 px-3 pb-3">
               Israel Jelaskan Serangan ke Kantor Berita AP dan Al-Jazeera
             </div>
           </li>
-          <li className="flex flex-none flex-col w-56 bg-gray-100 overflow-hidden">
+          <li className="flex flex-none flex-col bg-gray-100 w-60 mr-4 overflow-hidden">
             <img className="" src="assets/sub6.jpeg" alt=""></img>
             <div className="pt-2 px-3 pb-3">
               Gojek dan Tokopedia Resmi Merger Jadi GoTo
@@ -118,57 +118,53 @@ export function App() {
           </li>
         </ul>
         <div className="flex mb-16 gap-6">
-          <ul className="flex flex-col flex-1 gap-6">
-            <li className="flex flex-row items-center">
+          <ul className="flex flex-col flex-1 text-sm md:text-base mr-4">
+            <li className="flex flex-col md:flex-row items-center mb-6 md:mr-10">
               <img
-                className="flex-none w-44"
+                className="flex-none w-full md:w-44 mb-2 md:mb-0"
                 src="assets/md1.jpeg"
                 alt=""
               ></img>
-              <div className="leading-snug text-lg mx-4">
-                <a href="/" className="text-gray-700 font-semibold">
-                  3 Calon Lawan Charles Oliveira di UFC
-                </a>
-              </div>
+              <a href="/" className="text-gray-600 font-semibold mx-4">
+                3 Calon Lawan Charles Oliveira di UFC
+              </a>
             </li>
-            <li className="flex flex-row items-center">
+            <li className="flex flex-col md:flex-row items-center mb-6 md:mr-10">
               <img
-                className="flex-none w-44"
+                className="flex-none w-full md:w-44 mb-2 md:mb-0"
                 src="assets/md2.jpeg"
                 alt=""
               ></img>
-              <div className="leading-snug text-lg mx-4">
-                <a href="/" className="text-gray-700 font-semibold">
-                  Man Utd vs Fulham: Solskjaer Harap Suporter Tak Rusuh
-                </a>
-              </div>
+              <a href="/" className="text-gray-600 font-semibold mx-4">
+                Man Utd vs Fulham: Solskjaer Harap Suporter Tak Rusuh
+              </a>
             </li>
-            <li className="flex flex-row items-center">
+            <li className="flex flex-col md:flex-row items-center mb-6 md:mr-10">
               <img
-                className="flex-none w-44"
+                className="flex-none w-full md:w-44 mb-2 md:mb-0"
                 src="assets/md3.jpeg"
                 alt=""
               ></img>
-              <div className="leading-snug text-lg mx-4">
-                <a href="/" className="text-gray-700 font-semibold">
-                  Hadapi Lonjakan Kasus Covid, DKI Siapkan Ranjang Isolasi-ICU
-                </a>
-              </div>
+              <a href="/" className="text-gray-600 font-semibold mx-4">
+                Hadapi Lonjakan Kasus Covid, DKI Siapkan Ranjang Isolasi-ICU
+              </a>
             </li>
-            <li className="flex flex-row items-center">
-              <img className="flex-none w-44" src="assets/md4.jpg" alt=""></img>
-              <div className="leading-snug text-lg mx-4">
-                <a href="/" className="text-gray-700 font-semibold">
-                  AS Selidiki Penyakit Misterius yang Muncul Dekat Gedung Putih
-                </a>
-              </div>
+            <li className="flex flex-col md:flex-row items-center mb-6 md:mr-10">
+              <img
+                className="flex-none w-full md:w-44 mb-2 md:mb-0"
+                src="assets/md4.jpg"
+                alt=""
+              ></img>
+              <a href="/" className="text-gray-600 font-semibold mx-4">
+                AS Selidiki Penyakit Misterius yang Muncul Dekat Gedung Putih
+              </a>
             </li>
           </ul>
-          <div className="flex flex-col w-64">
+          <div className="flex flex-col w-32 md:w-64">
             <ul className="flex flex-col gap-4">
-              <li className="w-full bg-gray-200 h-64">iklan</li>
-              <li className="w-full bg-gray-200 h-64">iklan</li>
-              <li className="w-full bg-gray-200 h-64">iklan</li>
+              <li className="w-full bg-gray-200 h-32 md:h-64 mb-4">iklan</li>
+              <li className="w-full bg-gray-200 h-32 md:h-64 mb-4">iklan</li>
+              <li className="w-full bg-gray-200 h-32 md:h-64 mb-4">iklan</li>
             </ul>
           </div>
         </div>
