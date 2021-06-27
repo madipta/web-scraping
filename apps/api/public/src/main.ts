5b73ad68ad731209587b9697283c82c6219ad71b
madipta <madipta@gmail.com>
2021-06-27 23:07:23 +0700

    set public api url env var

--- apps/api/public/src/main.ts
+++ apps/api/public/src/main.ts
@@ -10,11 +10,10 @@ import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = "api";
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  app.enableCors();
+  const port = process.env.API_PUBLIC_PORT;
   await app.listen(port, () => {
-    Logger.log("Listening at http://localhost:" + port + "/" + globalPrefix);
+    Logger.log("Listening at http://localhost:" + port + "/");
   });
 }
 
