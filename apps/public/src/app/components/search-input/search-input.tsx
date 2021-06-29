4c74536a3c806a1ac0bce31b62bb01dd0c7405ea
madipta <madipta@gmail.com>
2021-06-29 21:42:54 +0700

    ui redesign

--- apps/public/src/app/components/search-input/search-input.tsx
+++ apps/public/src/app/components/search-input/search-input.tsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { ReactComponent as SearchIcon } from '../../icons/search.svg';
 
 /* eslint-disable-next-line */
 export interface SearchInputProps {}
 
 export function SearchInput(props: SearchInputProps) {
   return (
-    <div className="col-start-1 col-end-13 flex flex-nowrap w-full md:w-7/12 overflow-hidden rounded-r-full mx-auto mb-12">
+    <div className="col-start-1 col-end-13 flex flex-nowrap w-10/12 sm:w-7/12 md:w-6/12 overflow-hidden rounded-r-full mx-auto mb-12">
       <input
         type="search"
         placeholder="What are you searching for?"
         className="flex-1 focus:outline-none min-w-0 px-3 py-2 rounder-l-2 border border-r-0 border-gray-200"
       ></input>
-      <button className="flex-none bg-yellow-500 text-white font-medium pl-3 pr-4">
-        Search
+      <button className="flex-none flex items-center bg-yellow-500 text-white text-xs font-medium pl-3 pr-4 sm:pr-6">
+        <SearchIcon></SearchIcon><p className="ml-1 hidden sm:inline-block">Search</p>
       </button>
     </div>
   );
