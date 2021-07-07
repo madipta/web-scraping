08f19322281cb3cd14f8b66e022e06e8a9cbe319
madipta <madipta@gmail.com>
2021-07-07 19:48:13 +0700

    add orm

--- apps/api/public/src/app/app.service.ts
+++ apps/api/public/src/app/app.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Content } from "@web-scraping/orm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(Content)
+    private readonly contentRepo: Repository<Content>
+  ) {}
+
   search(searchText: string): { message: string } {
     return { message: searchText };
   }
