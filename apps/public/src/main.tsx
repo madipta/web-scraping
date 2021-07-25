bb48d47ac20b476a36b9ec1d36d705e52d252986
madipta <madipta@gmail.com>
2021-07-25 21:02:49 +0700

    remove apollo graphql

--- apps/public/src/main.tsx
+++ apps/public/src/main.tsx
@@ -1,29 +1,14 @@
-import {
-  ApolloClient,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-} from "@apollo/client";
 import React from "react";
 import { StrictMode } from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/app";
 
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:3000/graphql",
-  }),
-  cache: new InMemoryCache(),
-});
-
 ReactDOM.render(
   <StrictMode>
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ApolloProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
   document.getElementById("root")
 );
