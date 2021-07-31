b013908382805d8325589fdbd5da98aff8420cfd
madipta <madipta@gmail.com>
2021-07-31 23:01:24 +0700

    load more on scroll

--- apps/public/src/app/main/main.tsx
+++ apps/public/src/app/main/main.tsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import SearchInput from "../components/search-input/search-input";
 import SearchResult from "../components/search-result/search-result";
 import useSearch from "./search";
 
 export function Main() {
-  const search = useSearch();
+  const { loading, result, hasMore, setQuery, page, setPage } = useSearch();
   const onSearch = (text) => {
-    search.setQuery(text);
-    search.setPage(1);
+    setQuery(text);
+    setPage(1);
   };
+  const containerRef = useRef();
+  useEffect(() => {
+    const current = containerRef.current;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (loading || !hasMore) return;
+        if (entries[0].isIntersecting) {
+          setPage(page + 1);
+        }
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 1.0,
+      }
+    );
+    if (current) observer.observe(current)
+    return () => {
+      if(current) observer.unobserve(current)
+    }
+  }, [containerRef, hasMore, loading, page, setPage]);
   return (
     <div className="auto-rows-max col-start-1 col-end-13 grid grid-cols-12">
       <SearchInput search={onSearch}></SearchInput>
-      <SearchResult result={search.result}></SearchResult>
+      <SearchResult result={result}></SearchResult>
+      <div ref={containerRef}></div>
     </div>
   );
 }
--- apps/public/src/app/main/search.tsx
+++ apps/public/src/app/main/search.tsx
@@ -2,7 +2,8 @@ import { useCallback, useEffect, useState } from "react";
 import { ISearchResult } from "../components/search-result/search-result";
 
 export default function useSearch(currentSearch = "") {
-  const [hasMore, setHasMore] = useState(true);
+  const pagesize = 20;
+  const [hasMore, setHasMore] = useState(false);
   const [loading, setLoading] = useState(false);
   const [query, setQuery] = useState(currentSearch);
   const [page, setPage] = useState(1);
@@ -17,9 +18,20 @@ export default function useSearch(currentSearch = "") {
       `http://localhost:3000/search?q=${encodeURIComponent(query)}&page=${page}`
     );
     const result = (await response.json()) as ISearchResult[];
+    setResult((prev) => {
+      const map = new Map();
+      const data = [];
+      const union = [...prev, ...result];
+      for (const item of union) {
+        if (!map.has(item.id)) {
+          map.set(item.id, true);
+          data.push(item);
+        }
+      }
+      return data;
+    });
+    setHasMore(result && result.length === pagesize);
     setLoading(false);
-    setHasMore(!result || !result.length);
-    setResult((prev) => [...prev, ...result]);
   }, [query, page]);
   useEffect(() => {
     setResult([]);
