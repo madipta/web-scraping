4e5ac0a4c8f21557bbc7d2c5af3654e80dccc4d2
madipta <madipta@gmail.com>
2021-08-02 23:30:25 +0700

    remove react-query & styled-component package

--- apps/public/src/app/app.tsx
+++ apps/public/src/app/app.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
 import AppHeader from "./components/app-header/app-header";
 import Main from "./pages/main/main";
 
-const queryClient = new QueryClient();
-
 export function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="grid grid-cols-12 font-sans text-sm px-3 sm:px-5 md:px-8 py-24 mx-auto">
-        <AppHeader></AppHeader>
-        <Main></Main>
-      </div>
-    </QueryClientProvider>
+    <div className="grid grid-cols-12 font-sans text-sm px-3 sm:px-5 md:px-8 py-24 mx-auto">
+      <AppHeader></AppHeader>
+      <Main></Main>
+    </div>
   );
 }
 
