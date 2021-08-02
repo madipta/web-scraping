cba66cbedf5285bf6ab4e00a182764a2d3cdb109
madipta <madipta@gmail.com>
2021-08-02 23:22:51 +0700

    tailwindcscs config

--- apps/public/tailwind.config.js
+++ apps/public/tailwind.config.js
@@ -2,12 +2,9 @@ const { join } = require("path");
 
 module.exports = {
   presets: [require("../../tailwind.config.base.js")],
-  purge: {
-    enabled: process.env.NODE_ENV === "production",
-    content: [
-      join(__dirname, "src/app/app.tsx"),
-      join(__dirname, "src/app/components/**/*.{js,ts,jsx,tsx}"),
-      join(__dirname, "src/app/pages/**/*.{js,ts,jsx,tsx}"),
-    ],
-  },
+  purge: [
+    join(__dirname, "src/app/app.tsx"),
+    join(__dirname, "src/app/components/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "src/app/pages/**/*.{js,ts,jsx,tsx}"),
+  ],
 };
