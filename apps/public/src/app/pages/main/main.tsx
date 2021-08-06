56ffa52a33ea95858e2ee68092cefd19bdd3b3ff
madipta <madipta@gmail.com>
2021-08-06 22:05:32 +0700

    add go up button

--- apps/public/src/app/pages/main/main.tsx
+++ apps/public/src/app/pages/main/main.tsx
@@ -2,16 +2,25 @@ import React, { useEffect, useRef } from "react";
 import SearchInput from "../../components/search-input/search-input";
 import SearchResult from "../../components/search-result/search-result";
 import useSearch from "./search";
+import { ReactComponent as UpIcon } from "../../icons/chevron-up.svg";
 
 export function Main() {
-  const { loading, hasMore, setQuery, page, setPage, result, error } = useSearch();
+  const {
+    loading,
+    hasMore,
+    setQuery,
+    page,
+    setPage,
+    result,
+    error,
+  } = useSearch();
   const onSearch = (text) => {
     setQuery(text);
     setPage(1);
   };
-  const containerRef = useRef();
+  const bottomElRef = useRef();
   useEffect(() => {
-    const current = containerRef.current;
+    const current = bottomElRef.current;
     const observer = new IntersectionObserver(
       (entries) => {
         if (loading || !hasMore) return;
@@ -25,17 +34,33 @@ export function Main() {
         threshold: 1.0,
       }
     );
-    if (current) observer.observe(current)
+    if (current) observer.observe(current);
     return () => {
-      if(current) observer.unobserve(current)
-    }
-  }, [containerRef, hasMore, loading, page, setPage]);
+      if (current) observer.unobserve(current);
+    };
+  }, [bottomElRef, hasMore, loading, page, setPage]);
   return (
     <div className="auto-rows-max col-start-1 col-end-13 grid grid-cols-12">
       <SearchInput search={onSearch}></SearchInput>
       <SearchResult result={result}></SearchResult>
-      <div ref={containerRef}></div>
-      { error && <div className="col-start-1 col-end-13 text-red-500 text-sm text-center mt-5">Something error happen..</div> }
+      <div ref={bottomElRef}></div>
+      {error && (
+        <div className="col-start-1 col-end-13 text-red-500 text-sm text-center mt-5">
+          Something error happen..
+        </div>
+      )}
+      {result && result.length > 20 && (
+        <div className="fixed bottom-0 right-0 mb-3 mr-5">
+          <button
+            onClick={() => {
+              window.scrollTo(0, 0);
+            }}
+            className="bg-gray-200 text-yellow-600 w-12 p-3 rounded-full focus:outline-none opacity-75"
+          >
+            <UpIcon></UpIcon>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
