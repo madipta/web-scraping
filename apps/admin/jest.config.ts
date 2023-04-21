3601f9460438ea3513a9b0b2678b729fbf590885
Madipta <madipta@gmail.com>
2023-04-21 20:01:05 +0700

    nx upgrade 15.9.2

--- apps/admin/jest.config.ts
+++ apps/admin/jest.config.ts
@@ -1,15 +1,24 @@
 /* eslint-disable */
 export default {
   displayName: "admin",
-  
   setupFilesAfterEnv: ["<rootDir>/src/test-setup.ts"],
-  globals: {
-    "ts-jest": {
-      
-      stringifyContentPathRegex: "\\.(html|svg)$",
-      "tsconfig": "<rootDir>/tsconfig.spec.json"
-    },
-  },
-  coverageDirectory: "../../coverage/apps/admin","snapshotSerializers": ["jest-preset-angular/build/serializers/no-ng-attributes","jest-preset-angular/build/serializers/ng-snapshot","jest-preset-angular/build/serializers/html-comment"],"transform": {"^.+\\.(ts|js|html)$":"jest-preset-angular"},"preset": "..\\..\\jest.preset.ts"
-  
+  globals: {},
+  coverageDirectory: "../../coverage/apps/admin",
+  snapshotSerializers: [
+    "jest-preset-angular/build/serializers/no-ng-attributes",
+    "jest-preset-angular/build/serializers/ng-snapshot",
+    "jest-preset-angular/build/serializers/html-comment",
+  ],
+  transform: { "^.+\\.(ts|js|html)$": "jest-preset-angular" },
+  preset: "..\\..\\jest.preset.ts",
+  /* TODO: Update to latest Jest snapshotFormat
+   * By default Nx has kept the older style of Jest Snapshot formats
+   * to prevent breaking of any existing tests with snapshots.
+   * It's recommend you update to the latest format.
+   * You can do this by removing snapshotFormat property
+   * and running tests with --update-snapshot flag.
+   * Example: From within the project directory, run "nx test --update-snapshot"
+   * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
+   */
+  snapshotFormat: { escapeString: true, printBasicPrototype: true },
 };
--- libs/scrape-queue/jest.config.ts
+++ libs/scrape-queue/jest.config.ts
@@ -1,16 +1,27 @@
 /* eslint-disable */
 export default {
   displayName: "scrape-queue",
-  
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.spec.json",
-    },
-  },
+  globals: {},
   testEnvironment: "node",
   transform: {
-    "^.+\\.[tj]sx?$": "ts-jest",
+    "^.+\\.[tj]sx?$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+      },
+    ],
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../coverage/libs/scrape-queue","preset": "..\\..\\jest.preset.ts"
+  coverageDirectory: "../../coverage/libs/scrape-queue",
+  preset: "..\\..\\jest.preset.ts",
+  /* TODO: Update to latest Jest snapshotFormat
+   * By default Nx has kept the older style of Jest Snapshot formats
+   * to prevent breaking of any existing tests with snapshots.
+   * It's recommend you update to the latest format.
+   * You can do this by removing snapshotFormat property
+   * and running tests with --update-snapshot flag.
+   * Example: From within the project directory, run "nx test --update-snapshot"
+   * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
+   */
+  snapshotFormat: { escapeString: true, printBasicPrototype: true },
 };
