e85dc4c8bbe89f026f049634439f5f7e4b351a0a
Madipta <madipta@gmail.com>
2023-04-21 23:46:57 +0700

    upgrade tailwind

--- apps/public/tailwind.config.js
+++ apps/public/tailwind.config.js
@@ -2,7 +2,7 @@ const { join } = require("path");
 
 module.exports = {
   presets: [require("../../tailwind.config.base.js")],
-  purge: [
+  content: [
     join(__dirname, "src/app/app.tsx"),
     join(__dirname, "src/app/components/**/*.{js,ts,jsx,tsx}"),
     join(__dirname, "src/app/pages/**/*.{js,ts,jsx,tsx}"),
--- tailwind.config.base.js
+++ tailwind.config.base.js
@@ -1,10 +1,5 @@
 module.exports = {
-  // mode: "jit",
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
