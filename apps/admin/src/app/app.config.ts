df80db476bfe44780504e6ddd7b0f585961f92fc
Madipta <madipta@gmail.com>
2023-05-21 17:58:17 +0700

    @apollo apollo-angular graphql package update

--- apps/admin/src/app/app.config.ts
+++ apps/admin/src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig } from "@angular/core";
-import { enableProdMode, importProvidersFrom } from "@angular/core";
-import { AppApolloModule } from "./app.apollo.provider";
-import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideHttpClient } from "@angular/common/http";
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { AppApolloModule } from "./app.apollo.provider";
 import { provideRouter } from "@angular/router";
 import { appRoutes } from "./app.router";
+
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(AppApolloModule),
